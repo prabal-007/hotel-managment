b9a1a888db302b7847cf17fd3a42312bf1c5d765
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-11-12 23:46:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ frm1 = Frame(bg='red')
 Label(frm1,text='Hotel Management SYSTEM', font='arial 25 bold', padx='15', pady='5', bg='gold', relief=SUNKEN, border=4).pack(padx=10, pady=10, fill=X)
 frm1.pack(side=TOP, fill=X)
 staVar=StringVar()
-# ////////////////////////////////////////////////// ////////////////////
+# ////////////////////////////////////////////////// ///////////////////
 def submitOrder():
     screen.configure(state=NORMAL)
     screen.delete('1.0', END)
@@ -106,7 +106,7 @@ for item in southIndian:
 minifrm3.pack(padx=5,pady=5,anchor='e')
 
 mainfrm.pack(pady=2,padx=1)
-    # ///////////////////////////////////////////////////////////////////////////
+    # //////////////////////////////////////////////////////////////////////////
 
 bottomfrm = Frame(rightfrm,relief=FLAT, border='8',background='red')
 Button(bottomfrm,text='BACK', font='arial 15 bold',relief=SUNKEN, command=NONE).pack(padx=10,side=LEFT)
